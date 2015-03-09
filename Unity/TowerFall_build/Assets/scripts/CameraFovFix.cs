73a42442509d318bbb52dfb5a0e0b07913aba9da
Pepijn Willekens <pepijnwillekens@gmail.com>
2015-03-09 12:40:59 +0100

    patch6

---
+++ Unity/TowerFall_build/Assets/scripts/CameraFovFix.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFovFix : MonoBehaviour
+{
+  public float SmallestFOV = 60;
+	
+	void Update ()
+	{
+	    float aspectRatio = Screen.width/(float) Screen.height;
+	    if (aspectRatio > 1)
+	    {
+	      camera.fieldOfView = SmallestFOV/aspectRatio;
+	    }
+	    else
+	    {
+	      camera.fieldOfView = SmallestFOV;
+	    }
+	  
+	}
+}
